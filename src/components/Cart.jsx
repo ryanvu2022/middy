8806308827011b33c6e4d8b8eb7f34ab516cd933
Thinh <ryanvu2022@gmail.com>
2022-11-16 12:46:30 -0500

    complete update quantity functionality in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -19,34 +19,63 @@ const Cart = () => {
       </header>
       {cart.length > 0 ? 
       <>
-        <div>
+        
           {cart.map(item => (
-            <article key={item.id} className="grid items-center grid-cols-cartgrid gap-x-6 my-6 mx-0">
+            <div key={item.id} className="grid items-center grid-cols-cartgrid gap-x-6 my-6 mx-0">
               <img src={item.image} alt={item.name} className="rounded w-24 h-24 object-cover" />
+
               <div>
                 <h4 className="text-lg font-medium mb-2 text-darkblue">{item.name}</h4>
                 <h4 className="font-medium mb-2 tracking-wider">${item.price}</h4>
-                <button className="font-medium tracking-wide hover:text-red-600" onClick={() => dispatch({
+                <button 
+                  className="font-medium tracking-wide hover:text-red-600" 
+                  onClick={() => dispatch({
                     type: "REMOVE_FROM_CART",
                     payload: item
-                  })}>Remove</button>
+                  })}
+                >
+                  Remove
+                </button>
               </div>
+
               <div>
-                <button className="text-darkblue"><ChevronUp /></button>
-                <p className="text-center text-xl leading-none">{item.quantity}</p>
-                <button className="text-darkblue" onClick={() => {
-                  if (item.quantity === 1) {
+                <button 
+                  className="text-darkblue"
+                  onClick={() => {                  
                     dispatch({
-                      type: "REMOVE_FROM_CART",
-                      payload: item
+                      type: "UPDATE_QUANTITY",
+                      payload: {
+                        id: item.id,
+                        quantity: item.quantity + 1
+                      }
                     })
-                  }
-                }}><ChevronDown /></button>
+                  }}
+                >
+                  <ChevronUp />
+                </button>
+
+                <p className="text-center text-xl leading-none relative bottom-1">{item.quantity}</p>
+
+                <button 
+                  className="text-darkblue" 
+                  onClick={() => {
+                    if (item.quantity > 1) {                      
+                      dispatch({
+                        type: "UPDATE_QUANTITY",
+                        payload: {
+                          id: item.id,
+                          quantity: item.quantity - 1
+                        }
+                      })}
+                  }}
+                >
+                  <ChevronDown />
+                </button>
               </div>
-            </article>
+            </div>
             
           ))}
-        </div>
+        
         <footer className="flex flex-col">
           <hr className="border border-transparent bg-darkblue"/>
           <div className="text-center mt-4">
